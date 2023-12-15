e5ff49309cdb0429b8b84a4c74894ae32c8b39e9
Kesehet <hamood.siddiqui@gmail.com>
2023-12-16 00:37:54 +0530

    minor changes ...

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from src.gmaps import Gmaps
 
-star_it = '''Help us reach 850 stars, and we'll break the GMaps 120 limit, giving you 150+ to 250+ potential customers per search.
-             Star us to make it happen ⭐! https://github.com/omkarcloud/google-maps-scraper/'''
 
 queries = Gmaps.Cities.India("masjid")[0:100]
 fields = [
