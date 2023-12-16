1e35acebbf8a2f0c713c8802bae48925da7743d9
Kesehet <hamood.siddiqui@gmail.com>
2023-12-16 06:51:00 +0530

    Update: iteration 0 with start value 1100

---
+++ run-loop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Starting value
+start=1100
+
+# Increment value
+increment=100
+
+# Number of iterations
+iterations=10
+
+for (( i=0; i<$iterations; i++ ))
+do
+    # Run your Python script
+    /c/path/to/python.exe main.py $((start + i * increment))
+
+    # Git add, commit, and push
+    git add .
+    git commit -m "Update: iteration $i with start value $((start + i * increment))"
+    git push origin main
+done
